5b9384d003c5d7cb441185b177b4fbc8224de071
NYDino <rfapp00@gmail.com>
2020-01-09 16:39:05 -0300

    :card_file_box: connecting database

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require("mongoose");
 
+// starting the App
 const app = express();
 
+// starting db
+mongoose.connect('mongodb://localhost:27017/bttrserver', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// first route
 app.get("/", (req, res) => {
     res.send("hello world!");
 })
