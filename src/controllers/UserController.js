dd04450daf071eda88b64523e9bbedb778deccc9
NYDino <rfapp00@gmail.com>
2020-01-10 16:40:00 -0300

    :sparkles: CRUD

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -10,10 +10,30 @@ module.exports = {
         return res.json(users);
     },
 
+    async show(req, res) {
+        const user = await User.findById(req.params.id);
+
+        return res.json(user);
+    },
+
     async store(req, res) {
         req.body.password = Bcrypt.hashSync(req.body.password, 10);
         const user = await User.create(req.body);
 
         return res.json(user);
+    },
+
+    async update(req, res) {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        })
+
+        return res.json(user)
+    },
+
+    async destroy(req, res) {
+        await User.findByIdAndRemove(req.params.id);
+
+        res.send();
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,9 @@ const routes = express.Router();
 const UserController = require('./controllers/UserController');
 
 routes.get("/users", UserController.index);
+routes.get("/users/:id", UserController.show);
 routes.post("/users", UserController.store);
+routes.put("/users/:id", UserController.update);
+routes.delete("/users/:id", UserController.destroy);
 
 module.exports = routes;
