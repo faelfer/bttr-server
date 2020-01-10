d20bb114f72000821f7715d266eff9564b9be965
NYDino <rfapp00@gmail.com>
2020-01-10 15:32:14 -0300

    :building_construction: file restructuring

--- server.js
+++ server.js
@@ -15,17 +15,7 @@ mongoose.connect(
 );
 requireDir("./src/models");
 
-const User = mongoose.model('User');
-
-// first route
-app.get("/", (req, res) => {
-    User.create({
-        email: "rfapp00@gmail.com",
-        username: "NYDino",
-        password: "123"
-    })
-
-    res.send("hello world!");
-})
+// routes
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
