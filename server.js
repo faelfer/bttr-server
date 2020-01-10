2daf46d0ba38344d72cadd44ac7afb781c85d3e9
NYDino <rfapp00@gmail.com>
2020-01-10 16:08:14 -0300

    :sparkles: record creation

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const requireDir = require('require-dir');
 
 // starting the App
 const app = express();
+app.use(express.json());
 
 // starting db
 mongoose.connect(
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Bcrypt = require("bcryptjs");
 
 const User = mongoose.model('User');
 
@@ -7,5 +8,12 @@ module.exports = {
         const users = await User.find();
 
         return res.json(users);
+    },
+
+    async store(req, res) {
+        req.body.password = Bcrypt.hashSync(req.body.password, 10);
+        const user = await User.create(req.body);
+
+        return res.json(user);
     }
 }
