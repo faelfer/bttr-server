e5436d68f6462e3b40a36887a917919f488dda09
NYDino <rfapp00@gmail.com>
2020-01-10 15:10:34 -0300

    :sparkles: creating user template

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // starting the App
 const app = express();
 
 // starting db
-mongoose.connect('mongodb://localhost:27017/bttrserver', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+    'mongodb://localhost:27017/bttrserver', 
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    }
+);
+requireDir("./src/models");
+
+const User = mongoose.model('User');
 
 // first route
 app.get("/", (req, res) => {
+    User.create({
+        email: "rfapp00@gmail.com",
+        username: "NYDino",
+        password: "123"
+    })
+
     res.send("hello world!");
 })
 
---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        lowercase: true,
+        trim: true,
+        required: true,
+    },
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+mongoose.model('User', UserSchema);
