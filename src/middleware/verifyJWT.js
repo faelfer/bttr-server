e6f219514fa49455ecf0246e762c194138d9c4b6
faelfer <rfapp00@gmail.com>
2020-08-27 23:13:37 -0300

    :sparkles: error message if the token is expired

--- src/middleware/verifyJWT.js
+++ src/middleware/verifyJWT.js
@@ -3,12 +3,18 @@ const jwt = require('jsonwebtoken');
 module.exports = function verifyJWT(req, res, next){
     var token = req.headers['authorization'];
     if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
-    
-    jwt.verify(token, 'bttr-server', function(err, decoded) {
-      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-      
-      // se tudo estiver ok, salva no request para uso posterior
+
+    try {
+      var decoded = jwt.verify(token, 'bttr-server');
+
       req.userId = decoded.id;
       next();
-    });
+
+    } catch(err) {
+      console.log("verifyJWT | err: ", err);
+      if (err.name == "TokenExpiredError") return res.status(401).send({ auth: false, message: 'JWT token has expired.' });
+
+      return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+
+    }
   }
