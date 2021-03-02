47e2ac75e4bc628f364e6b9dafc9b62d141cdef4
faelfer <rfapp00@gmail.com>
2021-03-02 01:50:22 -0300

    :recycle: replication of sending email in forgot and reset my password

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,10 @@ const UserController = require('./controllers/UserController');
 const ProgressController = require('./controllers/ProgressController');
 const ProgressHistoricController = require('./controllers/ProgressHistoricController');
 
-routes.get("/user/:id/profile", verifyJWT, UserController.profile);
+routes.get("/user/profile", verifyJWT, UserController.profile);
 routes.post("/user/sign_up", UserController.signUp);
 routes.put("/user/:id", verifyJWT, UserController.update);
-routes.delete("/user/:id", verifyJWT, UserController.destroy);
+routes.delete("/user", verifyJWT, UserController.destroy);
 routes.post("/user/sign_in", UserController.signIn);
 routes.post("/user/forgot_password", UserController.forgotPassword);
 routes.post("/user/redefine_password", verifyJWT, UserController.redefinePassword);
@@ -17,13 +17,12 @@ routes.get("/progress", verifyJWT, ProgressController.index);
 routes.get("/progress/:id", verifyJWT, ProgressController.show);
 routes.post("/progress", verifyJWT, ProgressController.store);
 routes.put("/progress/:id", verifyJWT, ProgressController.update);
-routes.put("/progress_sum/:id", verifyJWT, ProgressController.progressSum);
+routes.put("/progress/:id/add_minutes", verifyJWT, ProgressController.addMinutes);
 routes.delete("/progress/:id", verifyJWT, ProgressController.destroy);
-routes.get("/progress_month/:date", verifyJWT, ProgressController.progressMonth);
-routes.get("/progress_overview_month", verifyJWT, ProgressController.progressOverviewMonth);
 
 routes.get("/progress_historic", verifyJWT, ProgressHistoricController.index);
 routes.get("/progress_historic/:id", ProgressHistoricController.show);
+routes.get("/progress_historic/filter_by_progress/:id", ProgressHistoricController.filterByProgress);
 routes.post("/progress_historic", ProgressHistoricController.store);
 routes.put("/progress_historic/:id", verifyJWT, ProgressHistoricController.update);
 routes.delete("/progress_historic/:id", verifyJWT, ProgressHistoricController.destroy);
